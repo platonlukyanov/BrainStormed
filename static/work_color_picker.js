aa0fd0e1b6c7cae0f32176eab7b7954b666b6df1
platonlukyanov <76910837+platonlukyanov@users.noreply.github.com>
2021-04-17 23:31:00 +0300

    New under menu

--- static/work_color_picker.js
+++ static/work_color_picker.js
@@ -3,11 +3,12 @@ var hueb = new Huebee(colorInput, {
 
     setBGColor: true,
     hue0: 0,
-    hues: 1,
+    hues: 6,
     shades: 0,
     customColors: ["#ffb3ba", "#ffdfba", "#ffffba", "#baffc9", "#bae1ff"],
 
 });
+
 hueb.on('change', function (color, hue, sat, lum) {
     document.getElementById("idea-form").style.backgroundColor = color;
 
